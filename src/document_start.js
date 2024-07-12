1e53be6681ac5437d6a78c9ce847a4d50b04d42b
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-07-12 14:16:47 -0500

    Add a button on top
    Note the button is a little weird and not visible

--- src/document_start.js
+++ src/document_start.js
@@ -1,13 +1,14 @@
 console.log('document_start.js loaded!');
 
-document.addEventListener('keypress', function(event) {
-    console.log('Key pressed:', event.key);
-    if (event.key === 'Enter') {
-        var port = chrome.runtime.connect({name: "document_start"});
-        port.onMessage.addListener(function(message) {
-            console.log(message);
-            alert(message.message);
-        });
-        port.postMessage({greeting: "hello"});
-    }
-});
+// These are various IDs in the page, if they ever change
+// the extension would stop working and need updates.
+const pr_description_label_id = '__bolt-pr-description-label';
+
+var pr_description_label = document.getElementById(pr_description_label_id);
+if (pr_description_label) {
+    console.log('Found PR description label:', pr_description_label_id);
+
+    pr_description_label.parentElement.innerHTML += '<button aria-label="Ask an AI" aria-roledescription="button" class="no-margin bolt-header-command-item-button bolt-button bolt-icon-button enabled subtle icon-only bolt-focus-treatment" id="__bolt-ask-ai" role="menuitem" type="button"><span class="fluent-icons-enabled"><span class="left-icon flex-noshrink fabric-icon .ms-Icon--Help medium"></span></span></button>';
+} else {
+    console.log('Did not find PR description label');
+}
