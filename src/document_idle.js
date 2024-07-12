e0a7cd3ba0be590a02469d10880c9d5d783c915c
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-07-12 15:06:47 -0500

    Retrieve values from page

--- src/document_idle.js
+++ src/document_idle.js
@@ -8,6 +8,12 @@ const ai_button_id = '__bolt-ask-ai';
 const pr_description_label_id = '__bolt-pr-description-label';
 const pr_description_textarea_id = '__bolt-textfield-input-2';
 
+// These are also names that if AzDO changes, would break
+const source_branch_name = 'sourceRef';
+const target_branch_name = 'targetRef';
+const source_repo_name = 'sourceRepositoryId';
+const target_repo_name = 'targetRepositoryId';
+
 // Wait 1 second for the page to load, then attach to it
 setTimeout(attachToPage, 1000);
 
@@ -21,7 +27,17 @@ function attachToPage() {
         var ask_ai_button = getElementAndLog(ai_button_id);
         ask_ai_button.onclick = function() {
             console.log('Ask an AI button clicked!');
-            pr_description_textarea.value = 'AI was here!\n' + pr_description_textarea.value;
+
+            const params = new URLSearchParams(window.location.search);
+            const source_branch = params.get(source_branch_name);
+            const target_branch = params.get(target_branch_name);
+            const source_repo = params.get(source_repo_name);
+            const target_repo = params.get(target_repo_name);
+
+            pr_description_textarea.focus();
+            pr_description_textarea.value +=
+                '\nAI was here, we could send a payload such as:\n' +
+                `{ "source_branch": "${source_branch}", "target_branch": "${target_branch}", "source_repo": "${source_repo}", "target_repo": "${target_repo}" }`;
         };
     }
 }
